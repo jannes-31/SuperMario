13539f62d76b77107da31699a42ac0e4d37d11b2
jannes-31 <jannesgregorhoppe@gmail.com>
2026-02-19 13:12:50 +0100

    Update Welt.java

--- Welt.java
+++ Welt.java
@@ -5,32 +5,41 @@ public class Welt
 {
     View fenster;
     Picture marioStehend, coin, pipe, groundBlock, luckyBlock, stair, brick, marioPic, busch;
+    Picture[] platformen;
+    Rectangle pHitbox;
     Sprite mario;
-    Platform platform, platform1;
     Rectangle mHitbox;
     Color hintergrund;
-
+    Sprite[] platform;
     
     Text geschwindigkeit;
     
     
-    double geschwY   = 0;
+    double geschwY  = 0;
     float mX        = 400;
     float mY        = 100;
     float mBreite   = 50;        
     float mHoehe    = 50;  
-
+    double platformX = 700, platformY = 350;
+    
     Welt()
     {
         hintergrund = new Color(99,149,238);
 
         fenster = new View(1200, 800);
         fenster.setBackgroundColor(hintergrund);
-
-        platform = new Platform(650,550,150,20);
-        platform1 = new Platform(50,500,150,20);
-
-        luckyBlock = new Picture(450, 600, 50, 50, "LuckyBlock.png");
+        
+        
+        platformen = new Picture[3];
+        platform = new Sprite[3];
+        
+        platformen[0] = new Picture(650,550,150,20,"Platform.png");
+        platformen[1] = new Picture(50,500,150,20,"Platform.png");
+        platformen[2] = new Picture(platformX, platformY, 150, 20,"Platform.png");
+        
+        
+        luckyBlock = new Picture(450, 600, 50, 50, "LuckyBlock.png");   
+        luckyBlock = new Picture(500, 400, 50, 50, "LuckyBlock.png");
 
         pipe = new Picture(950,600,120,150,"Pipe.png");
 
@@ -42,26 +51,25 @@ public class Welt
             mario = new Sprite(mHitbox);
             mario.add(marioPic);
         
-        
+        pHitbox = new Rectangle(platformX,platformY,150,20);
+        platform[1] = new Sprite(pHitbox);
+        platform[2].add(platformen[2]);
+            
+            
+            
+            
         geschwindigkeit = new Text(200,100,Double.toString(geschwY));
         Thread update = new Thread(() -> 
         {
             while(true)
             {
-            
-                if(fenster.keyUpPressed()&& geschwY >-5)
+                platformen[2].move(5,0);
+                platformX = platformen[2].getShapeX();
+                if(platformX > 1000 && platformX < 700)
                 {
-                    geschwY -= 2;
-                    
+                    platformen[2].move(-5,0);
                 }
-                geschwindigkeit.setText(Double.toString(geschwY));
-                if(geschwY < 0.1)
-                {
-                    geschwY += 1;
-                }
-                mY += geschwY;
-                mario.move(0,mY);
-                fenster.wait(100);
+                
                 try
                 {
                     Thread.sleep(100);
@@ -89,7 +97,11 @@ public class Welt
         {
             brick = new Picture(i * 50, 600, 50, 50, "Brick.png");
         }
-
+        
+        for (int i = 7; i < 10; i++)
+        {
+            brick = new Picture(i * 50, 400, 50, 50, "Brick.png");
+        }
 
 
     }
